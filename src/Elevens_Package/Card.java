12b90b44c22ed77c3b31524c67901587b948964f
guptas5279 <guptas5279@CA-SU-F106-11.sduhsd.lan>
2019-03-04 09:01:59 -0800

    first push

---
+++ src/Elevens_Package/Card.java
@@ -0,0 +1,29 @@
+package Elevens_Package;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class Card
+{
+	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
+			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+
+	//instance variables
+		//String suit
+		//int face
+
+  	//constructors
+
+
+	// modifiers
+		//set methods
+
+
+  	//accessors
+		//get methods
+
+
+  	//toString
+
+ }
